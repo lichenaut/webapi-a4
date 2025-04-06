ed53dd57be8907b36e1c15847704a2106b0330d0
lichenaut <mt0n@tutanota.com>
2025-04-05 20:08:37 -0600

    hell

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ router
 
   // POST create new review (JWT protected)
   .post(authJwtController.isAuthenticated, async (req, res) => {
-    const { movieId, username, review, rating } = req.body;
+    let { movieId, username, review, rating } = req.body;
     movieId = Number(movieId);
 
     if (!movieId || !review || rating === undefined) {
